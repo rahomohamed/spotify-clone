96c7c72d61da83915e33c0c3c65d7761d19a46b3
rahomohamed <rahojmohamed@gmail.com>
2021-03-04 02:56:11 -0500

    render homepage using token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import Login from './Login';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-  return (
-    <div className="app">
-    <Login/>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    // got token from url 
+    const hash = getTokenFromUrl();
+    // stripped so no longer sits in url
+    window.location.hash = "";
+    let _token = hash.access_token;
+   // store token in state so its in memory
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
+  return <div className="app">{token ? <h1>Logged In</h1> : <Login />}</div>;
 }
 
 export default App;
