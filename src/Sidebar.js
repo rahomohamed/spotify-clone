0444f2b7d344d74cb77a13f5561336422366aab9
rahomohamed <rahojmohamed@gmail.com>
2021-03-04 05:59:14 -0500

    added icons to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar() {
   return (
@@ -9,6 +13,9 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+      <SidebarOption Icon={HomeIcon} option="Home" />
+      <SidebarOption Icon={SearchIcon} option="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
     </div>
   );
 }
